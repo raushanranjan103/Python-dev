2274f910b437a87be5f0a4ed8eebd7d70624fac0
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2021-12-21 15:01:50 +0530

    Add files via upload

---
+++ face detection using open cv Ranjan.py
@@ -0,0 +1,25 @@
+import cv2
+from matplotlib import pyplot as plt
+
+cap=cv2.VideoCapture(0)
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+while True:
+      ret,frame=cap.read()
+      gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        
+      if ret==False:  
+          continue
+      faces=face_cascade.detectMultiScale(gray_frame,1.3,5)
+    #   cv2.imshow("video Frame",frame)
+    #   cv2.imshow("Gray Frame",gray_frame)
+      for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+      cv2.imshow("video Frame",frame)
+
+      key_pressed=cv2.waitKey(1)& 0xFF
+      if key_pressed== ord('q'):
+         break
+            
+cap.realease()
+
+cv2.destroyALlWindows()
